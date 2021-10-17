49309bd82843428b7452c0d2771e7d1e0dd0f651
anlideer <643525796@qq.com>
2021-10-18 00:25:19 +0300

    spawn, slice...
    slicing function works all fine now (almost done!)

--- Assets/Scripts/MCubeManager.cs
+++ Assets/Scripts/MCubeManager.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 
 public class MCubeManager : MonoBehaviour
 {
-    public MovingCube temp;
     public static MovingCube CurrentCube { get; private set; }
     public static Vector3 LastPosition { get; set; }
+    public static float lastWidth;
+    public static bool isZ = true;
 
     private void Start()
     {
-        LastPosition = new Vector3(0, 0.55f, 0);
-        // tmp
-        CurrentCube = temp;
+        LastPosition = new Vector3(0, 0.45f, 0);
+        lastWidth = 1f;
+        SpawnCube(new Vector3(1f, 0.1f, 1f));
+    }
+
+    public static void SpawnCube(Vector3 scale)
+    {
+        GameObject obj = Instantiate(Resources.Load("MovingCube") as GameObject);
+        MovingCube mc = obj.GetComponent<MovingCube>();
+        Debug.Log("Lastposition" + LastPosition);
+        if (isZ)
+        {
+            scale.z = scale.x;
+            obj.transform.localScale = scale;
+            obj.transform.position = LastPosition + new Vector3(0, 0.1f, -2f);
+            Debug.Log("spawn " +obj.transform.position);
+            mc.moveDirection = new Vector3(0, 0, 1f);
+        }
+        else
+        {
+            scale.x = scale.z;
+            obj.transform.localScale = scale;
+            obj.transform.position = LastPosition + new Vector3(-2f, 0.1f, 0);
+            Debug.Log("spawn " + obj.transform.position);
+            mc.moveDirection = new Vector3(1f, 0, 0);
+        }
+
+        CurrentCube = mc;
+        isZ = !isZ;
     }
 }
--- Assets/Scripts/MovingCube.cs
+++ Assets/Scripts/MovingCube.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class MovingCube : MonoBehaviour
 {
     public float moveSpeed = 1f;
+    public Vector3 moveDirection = new Vector3(0, 0, 1);
 
     private bool isMoving = true;
+
+    private void OnEnable()
+    {
+        // random color
+        GetComponent<Renderer>().material.color = GetRandomColor();
+    }
 
     private void Start()
     {
@@ -16,42 +23,106 @@ public class MovingCube : MonoBehaviour
     private void Update()
     {
         if (isMoving)
-            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
         
     }
 
     // stop & slice...
     public void Stop()
     {
-        Debug.Log("Stop");
-        isMoving = false;
-
         // split
+        Vector3 diffVec = transform.position - MCubeManager.LastPosition;
+        Debug.Log("diff" + diffVec);
+        float maxInterval = MCubeManager.lastWidth / 2 + transform.localScale.x / 2;
+        if (Mathf.Abs(diffVec.x) >= maxInterval || Mathf.Abs(diffVec.z) >= maxInterval)
+            return; // TODO: FAIL
+        isMoving = false;
+        // special case: current cube is "in" the last cube
+        float minInterval = MCubeManager.lastWidth / 2 - transform.localScale.x / 2;
+        if (Mathf.Abs(diffVec.x) <= minInterval && Mathf.Abs(diffVec.z) <= minInterval)
+        {
+            MCubeManager.LastPosition = transform.position;
+            MCubeManager.lastWidth = transform.localScale.z;
+        }
+        else
+        {
+            if (diffVec.x != 0)
+                SliceInX();
+            else
+                SliceInZ();
+        }
+        MCubeManager.SpawnCube(transform.localScale);
+    }
+
+    private void SliceInX()
+    {
         Vector3 diffVec = transform.position - MCubeManager.LastPosition;
         Vector3 originalPos = transform.position;
         // the one remaining
         Vector3 scale = transform.localScale;
-        scale.x = 1 - Mathf.Abs(diffVec.x);
-        scale.z = 1 - Mathf.Abs(diffVec.z);
+        scale.x = scale.x/2 - Mathf.Abs(diffVec.x) + MCubeManager.lastWidth / 2;
         transform.localScale = scale;
         Vector3 pos = transform.position;
-        pos.x /= 2;
-        pos.z /= 2;
+        if (diffVec.x > 0)
+            pos.x = MCubeManager.LastPosition.x + (Mathf.Abs(diffVec.x) / 2 - transform.localScale.z / 4 + MCubeManager.lastWidth / 4);
+        else
+            pos.x = MCubeManager.LastPosition.x - (Mathf.Abs(diffVec.x) / 2 - transform.localScale.z / 4 + MCubeManager.lastWidth / 4);
+        Debug.Log("position " + pos);
         transform.position = pos;
         // the one dropping
         GameObject cube = Instantiate(Resources.Load("FakeCube") as GameObject);
-        Vector3 scale2 = cube.transform.localScale;
-        if (diffVec.x != 0)
-            scale2.x *= Mathf.Abs(diffVec.x);
-        else if (diffVec.z != 0)
-            scale2.z *= Mathf.Abs(diffVec.z);
+        cube.GetComponent<Renderer>().material.color = GetComponent<Renderer>().material.color;
+        Vector3 scale2 = transform.localScale;
+        scale2.x = transform.localScale.z / 2 + Mathf.Abs(diffVec.x) - MCubeManager.lastWidth / 2;
         cube.transform.localScale = scale2;
-        Vector3 pos2 = 0.5f * (diffVec.normalized + diffVec);
-        pos2.y = originalPos.y;
+        Vector3 pos2 = MCubeManager.LastPosition;
+        if (diffVec.x > 0)
+            pos2.x += Mathf.Abs(diffVec.x) / 2 + transform.localScale.z / 4 + MCubeManager.lastWidth / 4;
+        else
+            pos2.x -= Mathf.Abs(diffVec.x) / 2 + transform.localScale.z / 4 + MCubeManager.lastWidth / 4;
+        Debug.Log("position2 " + pos2);
         cube.transform.position = pos2;
 
-        // TODO: store position
+        MCubeManager.LastPosition = transform.position;
+        MCubeManager.lastWidth = transform.localScale.z;
     }
 
+    private void SliceInZ()
+    {
+        Vector3 diffVec = transform.position - MCubeManager.LastPosition;
+        Vector3 originalPos = transform.position;
+        // the one remaining
+        Vector3 scale = transform.localScale;
+        scale.z = scale.z / 2 - Mathf.Abs(diffVec.z) + MCubeManager.lastWidth / 2;
+        transform.localScale = scale;
+        Vector3 pos = transform.position;
+        if (diffVec.z > 0)
+            pos.z = MCubeManager.LastPosition.z + (Mathf.Abs(diffVec.z) / 2 - transform.localScale.x / 4 + MCubeManager.lastWidth / 4);
+        else
+            pos.z = MCubeManager.LastPosition.z - (Mathf.Abs(diffVec.z) / 2 - transform.localScale.x / 4 + MCubeManager.lastWidth / 4);
+        Debug.Log("position " + pos);
+        transform.position = pos;
+        // the one dropping
+        GameObject cube = Instantiate(Resources.Load("FakeCube") as GameObject);
+        cube.GetComponent<Renderer>().material.color = GetComponent<Renderer>().material.color;
+        Vector3 scale2 = transform.localScale;
+        scale2.z = transform.localScale.x / 2 + Mathf.Abs(diffVec.z) - MCubeManager.lastWidth / 2;
+        cube.transform.localScale = scale2;
+        Vector3 pos2 = MCubeManager.LastPosition;
+        if (diffVec.z > 0)
+            pos2.z += Mathf.Abs(diffVec.z) / 2 + transform.localScale.x / 4 + MCubeManager.lastWidth / 4;
+        else
+            pos2.z -= Mathf.Abs(diffVec.z) / 2 + transform.localScale.x / 4 + MCubeManager.lastWidth / 4;
+        Debug.Log("position2 " + pos2);
+        cube.transform.position = pos2;
+
+        MCubeManager.LastPosition = transform.position;
+        MCubeManager.lastWidth = transform.localScale.x;
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+    }
 
 }
